a195cba281a08a037c1a326ffc3b328861429018
Lucas Nogueira <75449523+lucaslovesdev@users.noreply.github.com>
2021-07-15 21:22:55 -0300

    update to apply the private modifier to set function of cpf properties

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,13 +1,18 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    var nome: String = "Lucas de "
+    var nome: String = "Lucas"
     var cpf: String = "123.456.789-10"
+    private set
+
 }
 
 fun main() {
     val lucas = Pessoa()
 
+    println(lucas)
     println(lucas.nome)
     println(lucas.cpf)
+
 }
+
