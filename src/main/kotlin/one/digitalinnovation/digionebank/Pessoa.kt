5dc4ce355d7cb3abba2fe046955def1b9ef1a4b1
Lucas Nogueira <75449523+lucaslovesdev@users.noreply.github.com>
2021-07-18 21:37:45 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,18 +1,9 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Lucas"
-    var cpf: String = "123.456.789-10"
-    private set
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
 
-    constructor()
 
-    fun pessoaInfo() = ("$nome e $cpf")
-
-}
-
-fun main() {
-    val lucas = Pessoa()
-    println(lucas.pessoaInfo())
-}
 
